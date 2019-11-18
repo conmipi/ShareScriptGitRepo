f721662fd3943a4cb2b248fa01d1a52c15506de5
conmipi <pickco@sheridancollege.ca>
2019-11-17 20:28:26 -0500

    Added Calendar homepage

--- ShareScript/Controllers/HomeController.cs
+++ ShareScript/Controllers/HomeController.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ShareScript.Models;
+using ShareScript.TagHelpers;
 
 namespace ShareScript.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        List<CalendarEvent> events = new List<CalendarEvent>();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,6 +22,11 @@ namespace ShareScript.Controllers
 
         public IActionResult Index()
         {
+            DateTime currentTime = DateTime.Today;
+            ViewBag.Month = currentTime.Month;
+            ViewBag.Year = currentTime.Year;
+            events.Add(new CalendarEvent("Test Event",currentTime,"info"));
+            ViewBag.CalendarEvents = events;
             return View();
         }
 
--- ShareScript/Startup.cs
+++ ShareScript/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace ShareScript
@@ -41,6 +43,12 @@ namespace ShareScript
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+                RequestPath = "/Content"
+            });
 
             app.UseRouting();
 
